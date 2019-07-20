da4919dcd8e946c99f9a47b1bbd7736a5c38a3f3
libinyl <1102109100@qq.com>
2019-07-20 13:19:27 +0800

    Add Service layer;add .gitignore

---
+++ src/main/java/com/heartgo/demo/service/AssetCNYCService.java
@@ -0,0 +1,13 @@
+package com.heartgo.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 人民币账户 service
+ */
+public class AssetCNYCService {
+    @Autowired
+    private AssetCNYCService assetCNYCService;
+
+
+}
---
+++ src/main/java/com/heartgo/demo/service/AssetCNYKService.java
@@ -0,0 +1,15 @@
+package com.heartgo.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 虚拟资产 service
+ */
+public class AssetCNYKService {
+    @Autowired
+    private AssetCNYKService assetCNYKService;
+
+
+
+
+}
